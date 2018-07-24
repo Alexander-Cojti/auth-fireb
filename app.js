10cf7d5b8653b934ddff9e1a5676c2b7356d4515
Oswaldinho24k <oswalfut_96@hotmail.com>
2018-07-24 00:50:55 -0500

    can modigy info and login without problems

---
+++ app.js
@@ -0,0 +1,105 @@
+var numberButton = document.getElementById('numberButton')
+var validateButton = document.getElementById('validate')
+
+var profile = document.getElementById('profile')
+var login = document.getElementById('login')
+
+
+//check if user is logged
+firebase.auth().onAuthStateChanged(function(user) {
+    if (user) {
+        localStorage.setItem('user', JSON.stringify(user.uid))                
+        showProfile(user.uid)
+    } else {
+        hideProfile()
+    }
+  });
+    
+
+//login number
+numberButton.addEventListener('click', function(){
+    var number = document.getElementById('number').value
+    console.log(number)
+    window.recaptchaVerifier = new firebase.auth.RecaptchaVerifier('recaptcha-container');
+    var appVerifier = window.recaptchaVerifier;
+    
+    
+    firebase.auth().signInWithPhoneNumber(number, appVerifier)
+        .then(function (confirmationResult) {                
+        console.log(confirmationResult)
+        
+        //confirm(code)
+        window.confirmationResult = confirmationResult                       
+
+        }).catch(function (error) {
+        // Error; SMS not sent
+        console.log(error)
+        // ...
+        });
+
+})
+
+validateButton.addEventListener('click', function(){
+    var code = document.getElementById('code').value
+    console.log(code)
+    window.confirmationResult.confirm(code)
+    .then(function (result) {
+    // User signed in successfully.
+        var user = result.user;
+        console.log(user)
+        firebase.database().ref('phone/users/'+user.uid).update({
+            uid:user.uid,
+            phoneNumber:user.phoneNumber
+        })
+        showProfile(user.uid)
+    
+    // ...
+    }).catch(function (error) {
+    // User couldn't sign in (bad verification code?)
+    // ...
+    console.log(error)
+    });
+})   
+
+function showProfile(uid){
+    profile.setAttribute('class', 'show')
+    login.setAttribute('class', 'hide')
+
+    firebase.database().ref('phone/users/'+uid).on('value', function(res){
+        console.log(res.val())
+        document.getElementById('phone').value = res.val().phoneNumber
+        document.getElementById('name').value = res.val().name
+        document.getElementById('instagram').value = res.val().instagram
+    })
+}
+
+function hideProfile(){
+    profile.setAttribute('class', 'hide')
+    login.setAttribute('class', 'show')
+}
+
+
+var editar = document.getElementById('editar')
+editar.addEventListener('click', function(){
+    var uid = JSON.parse(localStorage.getItem('user'))
+    console.log(uid)
+    var name = document.getElementById('name').value
+    var instagram = document.getElementById('instagram').value
+    firebase.database().ref('phone/users/'+uid).update({
+        name:name,
+        instagram:instagram
+    })
+})
+
+//logout
+var logout = document.getElementById('logout')
+logout.addEventListener('click', function(){
+    firebase.auth().signOut().then(function() {
+        hideProfile()
+      }).catch(function(error) {
+        console.log(error)
+      });
+})
+          
+
+    
